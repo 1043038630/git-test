3a6ca6a3f19f762d19dd141c7bac6771736c17a1
WangXinXi <1043038630@qq.com>
2022-02-13 17:10:11 +0800

    正常版本更新

--- src/main/java/com/dahua/GitTest.java
+++ src/main/java/com/dahua/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("你好git");
         System.out.println("你好git2");
         System.out.println("你好git3");
+        System.out.println("正常版本更新");
     }
 }
