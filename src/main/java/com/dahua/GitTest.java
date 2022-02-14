16f9252023474d6c60ad6b57f8a61d1dc1a6ded0
WangXinXi <1043038630@qq.com>
2022-02-14 10:07:49 +0800

    开发分支提交

--- src/main/java/com/dahua/GitTest.java
+++ src/main/java/com/dahua/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("你好git2");
         System.out.println("你好git3");
         System.out.println("正常版本更新");
+
+        System.out.println("开发版本更新");
     }
 }
